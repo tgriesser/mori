1c2bf781a7f635f92687864f0dc6ec4bc732fcaf
Tim Griesser <tgriesser10@gmail.com>
2014-09-07 15:20:12 -0400

    Fix project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Persistent Data Structures for JavaScript"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2322"]]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [com.cognitect/transit-cljs "0.8.182"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
